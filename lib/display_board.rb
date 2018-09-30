42ab8ca262be361e5c22e5c9a57b9eefcc07286e
Ryan Gill <rdgill95@gmail.com>
2018-09-30 17:39:14 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,6 +2,8 @@
 # out the current state.
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
- puts board
+  puts board[0,1,2]
+  puts board[3,4,5]
+  puts board[6,7,8]
 end
 

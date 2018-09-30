22b0ecb69310fa057a1951adfebf1bae1c222d49
Ryan Gill <rdgill95@gmail.com>
2018-09-30 17:40:01 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def display_board(board)
+def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
   puts board[0,1,2]
   puts board[3,4,5]
   puts board[6,7,8]
